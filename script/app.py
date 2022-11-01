dd6b9a485ba1d9917fd607dea2ce9bc9880ca1a9
Iydon Liang <liangiydon@gmail.com>
2022-11-01 15:30:00 +0800

    :sparkles: Use altair_chart insteaf of line_chart

--- script/app.py
+++ script/app.py
@@ -1,7 +1,21 @@
+import altair as alt
 import pandas as pd
 import streamlit as st
 
 from cache import meta, data
+
+
+def altair_chart(df: pd.DataFrame) -> alt.LayerChart:
+    # Reference: https://altair-viz.github.io/gallery/multiline_tooltip.html
+    df.index.name = '时间'
+    source = df.reset_index().melt('时间', var_name='标题', value_name='数值')
+    nearest = alt.selection(type='single', nearest=True, on='mouseover', fields=['时间'], empty='none')
+    line = alt.Chart(source).mark_line(interpolate='basis').encode(x='时间:T', y='数值:Q', color='标题:N')
+    selectors = alt.Chart(source).mark_point().encode(x='时间:T', opacity=alt.value(0)).add_selection(nearest)
+    points = line.mark_point().encode(opacity=alt.condition(nearest, alt.value(1), alt.value(0)))
+    text = line.mark_text(align='left', dx=5, dy=-5).encode(text=alt.condition(nearest, '数值:Q', alt.value(' ')))
+    rules = alt.Chart(source).mark_rule(color='gray').encode(x='时间:T').transform_filter(nearest)
+    return alt.layer(line, selectors, points, rules, text)
 
 
 keys = ['热度', '关注', '浏览', '赞同', '评论', '回答']
@@ -15,12 +29,11 @@ if submitted and titles:
     meta_selected = {title: meta_inversed[title] for title in titles}
     for key, tab in zip(keys, st.tabs(keys)):
         with tab:
-            st.line_chart(
-                pd.DataFrame.from_dict({
-                    title: data[link][key]
-                    for title, link in meta_selected.items()
-                }),
-            )
+            df = pd.DataFrame.from_dict({
+                title: data[link][key]
+                for title, link in meta_selected.items()
+            })
+            st.altair_chart(altair_chart(df), use_container_width=True)
     with st.expander('链接'):
         st.markdown('\n'.join(
             f'- [{title}]({link})'
